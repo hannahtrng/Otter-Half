5071574b239d04ba3f256c75d6a9768c698b9734
michelle-b7 <bai7michelle@gmail.com>
2024-11-04 16:12:41 -0800

    Adding main background

---
+++ Assets/StartButtonScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButtonScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    public void OnClick()
+    {
+        Debug.Log("no");
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
