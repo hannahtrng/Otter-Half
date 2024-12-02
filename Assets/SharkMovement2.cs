18534687edebd4e4230c7f56dfbe275eddcb1fa5
Hannah Truong <truonghannah1@gmail.com>
2024-12-01 18:13:59 -0800

    fixed hitbox + facing direction of shark
    read title

--- Assets/SharkMovement2.cs
+++ Assets/SharkMovement2.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class SharkMovement2 : MonoBehaviour
 {
-    public Transform otter; 
+    public Transform otter;
     public float speed = 2f;
 
     void Update()
@@ -12,6 +12,18 @@ public class SharkMovement2 : MonoBehaviour
             // Move toward the otter
             Vector2 direction = ((Vector2)otter.position - (Vector2)transform.position).normalized;
             transform.position = Vector2.MoveTowards(transform.position, otter.position, speed * Time.deltaTime);
+
+            // Flip the shark to face the otter
+            if (otter.position.x > transform.position.x)
+            {
+                // Face left
+                transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
+            else
+            {
+                // Face right
+                transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
         }
     }
 }
